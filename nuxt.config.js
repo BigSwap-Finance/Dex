f3326a66889d0294106c100730a6a2ee57f21853
BigSwap Finance <86725876+BigSwap-Finance@users.noreply.github.com>
2021-08-10 17:16:01 -0400

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   srcDir: '',
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
